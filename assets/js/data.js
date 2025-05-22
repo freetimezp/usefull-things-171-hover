f709c7e25aa0bc9ba59b8addbe8c96482dfcbac8
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-05-22 22:32:34 +0300

    03. usefull-things-171-hover. GSAP scroll & hover animation.

---
+++ assets/js/data.js
@@ -0,0 +1,131 @@
+export const awards = [
+  {
+    name: "Best site of the Year",
+    type: "Nominee",
+    project: "INNOVATE 2025",
+    label: "Awwwards",
+  },
+  {
+    name: "Site of the day",
+    type: "Awwwards",
+    project: "BTNX - ZXRT",
+    label: "See Live",
+  },
+  {
+    name: "Site of the day",
+    type: "Awwwards",
+    project: "Open Video",
+    label: "See Live",
+  },
+  {
+    name: "Site of the day",
+    type: "Awwwards",
+    project: "Artisan Craft",
+    label: "See Live",
+  },
+  {
+    name: "Site of the day",
+    type: "Awwwards",
+    project: "Appetite Day",
+    label: "See Live",
+  },
+  {
+    name: "Site of the day",
+    type: "Awwwards",
+    project: "Red Dreams",
+    label: "See Live",
+  },
+  {
+    name: "Site of the day",
+    type: "Awwwards",
+    project: "Elves of Storm",
+    label: "See Live",
+  },
+  {
+    name: "Site of the day",
+    type: "Awwwards",
+    project: "Destiny in the Eye",
+    label: "See Live",
+  },
+  {
+    name: "Site of the day",
+    type: "Awwwards",
+    project: "The Missing Roses",
+    label: "See Live",
+  },
+  {
+    name: "Site of the day",
+    type: "Awwwards",
+    project: "The Years's Twins",
+    label: "See Live",
+  },
+  {
+    name: "Site of the day",
+    type: "Awwwards",
+    project: "Dream in the Kiss",
+    label: "See Live",
+  },
+  {
+    name: "Best Design",
+    type: "Awwwards",
+    project: "Last Touch",
+    label: "See Live",
+  },
+  {
+    name: "Best Design",
+    type: "Awwwards",
+    project: "Emperor of Waves",
+    label: "See Live",
+  },
+  {
+    name: "Best Design",
+    type: "Awwwards",
+    project: "Shards in the Light",
+    label: "See Live",
+  },
+  {
+    name: "Best Design",
+    type: "Awwwards",
+    project: "The Last Ice",
+    label: "See Live",
+  },
+  {
+    name: "Best Design",
+    type: "Awwwards",
+    project: "Son in the Sword",
+    label: "See Live",
+  },
+  {
+    name: "Best Design",
+    type: "Awwwards",
+    project: "Living Nothing",
+    label: "See Live",
+  },
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
